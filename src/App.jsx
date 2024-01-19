4a3187ac41a509258cd0b034d7515bfbb47fdb38
Lexlinkan <alexander-lindqvist@live.se>
2024-01-19 16:11:33 +0100

    Starting in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      klöscht
-    </h1>
-  )
-}
+const App = () => (
+  <main className="realtive">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      Camps and skills
+    </section>
+    <section className="padding-x py-10">
+      Subscribe
+    </section>
+    <section className="padding">
+      video 
+    </section>
+    <section className="padding">
+      utmana dig själv, Learn more knapp
+    </section>
+    <section className="padding">
+      våra metoder, learn more knapp
+    </section>
+    <section className="padding">
+      reviews
+    </section>
+    <section className="padding">
+      samarbeten
+    </section>
+    <section className="padding">
+      kontakta oss
+    </section>
+    <section className="padding">
+      Footer
+    </section>
+  </main>
+);
+
+export default App
